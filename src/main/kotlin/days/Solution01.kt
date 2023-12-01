06d87c04111153de92f39c842c912211d753fc34
Verulean <65474222+Verulean@users.noreply.github.com>
2023-12-01 02:16:42 -0500

    Update Solution01.kt
    Clean up implementation

--- src/main/kotlin/days/Solution01.kt
+++ src/main/kotlin/days/Solution01.kt
@@ -7,38 +7,19 @@ import adventOfCode.util.PairOf
 object Solution01 : Solution<List<String>>(AOC_YEAR, 1) {
     override fun getInput(handler: InputHandler) = handler.getInput(delimiter = "\n")
 
-    private val numbers = mapOf(
-            "one" to "1",
-            "two" to "2",
-            "three" to "3",
-            "four" to "4",
-            "five" to "5",
-            "six" to "6",
-            "seven" to "7",
-            "eight" to "8",
-            "nine" to "9"
-    )
+    private val numbers = (1..9).associateBy(Int::toString)
 
-    private fun parseLine(line: String, allowWords: Boolean): Int {
-        var digits = ""
-        line.forEachIndexed { i, c ->
-            if (c.isDigit()) {
-                digits += c
-            } else if (allowWords) {
-                for ((name, value) in numbers) {
-                    if (line.substring(i).startsWith(name)) {
-                        digits += value
-                        break
-                    }
-                }
-            }
-        }
-        return if (digits.isNotEmpty()) "${digits.first()}${digits.last()}".toInt() else 0
+    private val words = sequenceOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+            .mapIndexed { index, name -> name to index + 1 }
+            .toMap()
+
+    private fun parseLine(line: String, calibrationMap: Map<String, Int>): Int {
+        return "${calibrationMap[line.findAnyOf(calibrationMap.keys)?.second]}${calibrationMap[line.findLastAnyOf(calibrationMap.keys)?.second]}".toInt()
     }
 
     override fun solve(input: List<String>): PairOf<Int> {
-        val ans1 = input.sumOf { parseLine(it, false) }
-        val ans2 = input.sumOf { parseLine(it, true) }
+        val ans1 = input.sumOf { parseLine(it, numbers) }
+        val ans2 = input.sumOf { parseLine(it, numbers + words) }
         return ans1 to ans2
     }
 }
