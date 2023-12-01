84d1fa612e2f4b3cf5f450a4924650f024c8e282
Verulean <65474222+Verulean@users.noreply.github.com>
2023-12-01 04:34:11 -0500

    Update Solution01.kt

--- src/main/kotlin/days/Solution01.kt
+++ src/main/kotlin/days/Solution01.kt
@@ -7,20 +7,20 @@ import adventOfCode.util.PairOf
 object Solution01 : Solution<List<String>>(AOC_YEAR, 1) {
     override fun getInput(handler: InputHandler) = handler.getInput(delimiter = "\n")
 
-    private val numbers = (1..9).associateBy(Int::toString)
+    private val digits = (1..9).associateBy(Int::toString)
 
     private val words = sequenceOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
         .mapIndexed { index, name -> name to index + 1 }
         .toMap()
 
     private fun parseLine(line: String, calibrationMap: Map<String, Int>): Int {
-        fun getDigit(searchFunction: (Collection<String>) -> Pair<Int, String>?) = calibrationMap.getValue(checkNotNull(searchFunction(calibrationMap.keys)).second)
+        fun getDigit(find: (Collection<String>) -> Pair<Int, String>?) = find(calibrationMap.keys)?.second?.let(calibrationMap::getValue) ?: 0
         return getDigit(line::findAnyOf) * 10 + getDigit(line::findLastAnyOf)
     }
 
     override fun solve(input: List<String>): PairOf<Int> {
-        val ans1 = input.sumOf { parseLine(it, numbers) }
-        val ans2 = input.sumOf { parseLine(it, numbers + words) }
+        val ans1 = input.sumOf { parseLine(it, digits) }
+        val ans2 = input.sumOf { parseLine(it, digits + words) }
         return ans1 to ans2
     }
 }
