9331e2c0fa6476cd028c3945fac4040121cf468a
Verulean <65474222+Verulean@users.noreply.github.com>
2023-12-02 00:41:21 -0500

    Day 02 Solution

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,6 @@
 import adventOfCode.Solution
+import kotlin.system.measureNanoTime
+import kotlin.time.Duration.Companion.nanoseconds
 
 fun main(args: Array<String>) {
     args.map(String::toIntOrNull).forEach {
@@ -6,7 +8,10 @@ fun main(args: Array<String>) {
             val solution = Class.forName("days.Solution%02d".format(it)).kotlin.objectInstance
             if (solution is Solution<*>) {
                 println("Day $it:")
-                solution.run()
+                val duration = measureNanoTime {
+                    solution.run()
+                }
+                println("Finished execution in ${duration.nanoseconds}.")
             }
         } catch (_: ClassNotFoundException) {
         }
--- src/main/kotlin/days/Solution02.kt
+++ src/main/kotlin/days/Solution02.kt
@@ -2,11 +2,53 @@ package days
 
 import adventOfCode.*
 import adventOfCode.util.*
+import kotlin.math.max
 
 object Solution02 : Solution<List<String>>(AOC_YEAR, 2) {
+    enum class Color {
+        RED, GREEN, BLUE
+    }
+
+    private val colorMap = mapOf("red" to Color.RED, "green" to Color.GREEN, "blue" to Color.BLUE)
+
     override fun getInput(handler: InputHandler) = handler.getInput(delimiter = "\n")
 
-    override fun solve(input: List<String>): Pair<Any?, Any?> {
-        return Pair(null, null)
+    private fun parseGame(line: String): Sequence<Map<Color, Int>> {
+        return line
+            .split(": ")
+            .last()
+            .split("; ")
+            .asSequence()
+            .map { draws ->
+                draws
+                    .split(", ")
+                    .map { it.split(" ") }
+                    .associate { colorMap.getValue(it.last()) to it.first().toInt() }
+            }
+    }
+
+    private fun minimumCubes(game: Sequence<Map<Color, Int>>): Map<Color, Int> {
+        val minCubes = Color.entries
+            .associateWith { 0 }
+            .toMutableMap()
+        game.forEach { draws ->
+            draws.forEach { (color, number) ->
+                minCubes[color] = max(minCubes.getValue(color), number)
+            }
+        }
+        return minCubes
+    }
+
+    override fun solve(input: List<String>): PairOf<Int> {
+        var ans1 = 0
+        var ans2 = 0
+        input.map(::parseGame)
+            .map(::minimumCubes)
+            .forEachIndexed { index, minCubes ->
+                val (red, green, blue) = Color.entries.map(minCubes::getValue)
+                if (red <= 12 && green <= 13 && blue <= 14) ans1 += index + 1
+                ans2 += red * green * blue
+            }
+        return ans1 to ans2
     }
 }
