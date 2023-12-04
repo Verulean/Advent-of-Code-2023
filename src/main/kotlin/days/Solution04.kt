99da2e1ef229e2c5aa60c586da5330ddea7d630e
Verulean <65474222+Verulean@users.noreply.github.com>
2023-12-04 01:01:47 -0500

    Day 04 Solution

---
+++ src/main/kotlin/days/Solution04.kt
@@ -0,0 +1,37 @@
+package days
+
+import adventOfCode.InputHandler
+import adventOfCode.Solution
+import adventOfCode.util.PairOf
+
+object Solution04 : Solution<List<Int>>(AOC_YEAR, 4) {
+    override fun getInput(handler: InputHandler): List<Int> {
+        return handler.getInput("\n")
+            .map { it.substringAfter(": ").split(" | ") }
+            .map { halves ->
+                halves.map {
+                    it.split(' ')
+                        .mapNotNull(String::toIntOrNull)
+                        .toSet()
+                }
+            }
+            .map { it.reduce(Set<Int>::intersect) }
+            .map(Set<Int>::size)
+    }
+
+    override fun solve(input: List<Int>): PairOf<Int> {
+        val cardCounts = input.indices
+            .associateWith { 1 }
+            .toMutableMap()
+        val ans1 = input.mapIndexedNotNull cardLoop@{ i, overlaps ->
+            if (overlaps == 0) return@cardLoop null
+            val cardCount = cardCounts.getValue(i)
+            (i + 1..i + overlaps)
+                .filter(cardCounts.keys::contains)
+                .forEach { cardCounts.merge(it, cardCount, Int::plus) }
+            List(overlaps - 1) { 2 }.fold(1, Int::times)
+        }.sum()
+        val ans2 = cardCounts.values.sum()
+        return ans1 to ans2
+    }
+}
