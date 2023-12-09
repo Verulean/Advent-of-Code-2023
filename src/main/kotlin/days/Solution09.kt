28f6e395de205062aa3bda39399884bdf3b316e9
Verulean <65474222+Verulean@users.noreply.github.com>
2023-12-09 12:20:52 -0500

    Update Solution09.kt

--- src/main/kotlin/days/Solution09.kt
+++ src/main/kotlin/days/Solution09.kt
@@ -6,24 +6,25 @@ import adventOfCode.util.PairOf
 import adventOfCode.util.ints
 
 typealias Layer = List<Int>
-typealias Pyramid = List<Layer>
+typealias Sides = PairOf<Int>
+typealias Pyramid = List<Sides>
 
 object Solution09 : Solution<List<Layer>>(AOC_YEAR, 9) {
     override fun getInput(handler: InputHandler) = handler.getInput("\n").map(String::ints)
 
     private fun pascalify(base: Layer): Pyramid {
-        val pyramid = mutableListOf<Layer>()
+        val pyramid = mutableListOf<PairOf<Int>>()
         var currLayer = base
         while (currLayer.any { it != 0 }) {
-            pyramid.add(currLayer)
+            pyramid.add(currLayer.first() to currLayer.last())
             currLayer = currLayer.zip(currLayer.drop(1)).map { it.second - it.first }
         }
         return pyramid
     }
 
-    private val Pyramid.nextValue get() = this.sumOf { it.last() }
+    private val Pyramid.nextValue get() = this.sumOf(Sides::second)
 
-    private val Pyramid.prevValue get() = this.map { it.first() }.foldRight(0, Int::minus)
+    private val Pyramid.prevValue get() = this.map(Sides::first).foldRight(0, Int::minus)
 
     override fun solve(input: List<Layer>): PairOf<Int> {
         val pyramids = input.map(::pascalify)
