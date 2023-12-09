eea07852045cb25abf042a0028d8af9c7f34f856
Verulean <65474222+Verulean@users.noreply.github.com>
2023-12-09 18:35:00 -0500

    Update Solution09.kt

--- src/main/kotlin/days/Solution09.kt
+++ src/main/kotlin/days/Solution09.kt
@@ -6,28 +6,18 @@ import adventOfCode.util.PairOf
 import adventOfCode.util.ints
 
 typealias Layer = List<Int>
-typealias Pyramid = PairOf<List<Int>>
 
 object Solution09 : Solution<List<Layer>>(AOC_YEAR, 9) {
     override fun getInput(handler: InputHandler) = handler.getInput("\n").map(String::ints)
 
-    private fun pascalify(base: Layer): Pyramid {
-        val pyramid = mutableListOf<Int>() to mutableListOf<Int>()
-        var currLayer = base
-        while (currLayer.any { it != 0 }) {
-            pyramid.first.add(currLayer.first())
-            pyramid.second.add(currLayer.last())
-            currLayer = currLayer.zip(currLayer.drop(1)).map { it.second - it.first }
-        }
-        return pyramid
+    private fun predictValues(base: Layer): PairOf<Int> {
+        if (base.all { it == 0 }) return 0 to 0
+        val (next, prev) = predictValues(base.zipWithNext().map { it.second - it.first })
+        return base.last() + next to base.first() - prev
     }
 
-    private val Pyramid.nextValue get() = this.second.sum()
-
-    private val Pyramid.prevValue get() = this.first.foldRight(0, Int::minus)
-
     override fun solve(input: List<Layer>): PairOf<Int> {
-        val pyramids = input.map(::pascalify)
-        return pyramids.sumOf { it.nextValue } to pyramids.sumOf { it.prevValue }
+        operator fun PairOf<Int>.plus(other: PairOf<Int>) = this.first + other.first to this.second + other.second
+        return input.map(::predictValues).reduce(PairOf<Int>::plus)
     }
 }
